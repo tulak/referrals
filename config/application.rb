de063c81bdc087c99bdd7f29cd8938902bc9ff81
Filip Zachar <tulak45@gmail.com>
2020-04-05 23:38:41 +0200

    Compile graphiql assets

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Referrals
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
   end
 end
